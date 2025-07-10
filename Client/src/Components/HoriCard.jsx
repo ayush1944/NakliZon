f63b1a71d9db320b7aed93f4ed26d359210c4063
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-07-10 04:07:59 -0700

    Update HoriCard.jsx

--- Client/src/Components/HoriCard.jsx
+++ Client/src/Components/HoriCard.jsx
@@ -85,7 +85,7 @@ const HorizontalCardProduct = ({category, heading}) => {
                                 <p className='text-red-600 font-medium'>{ displayINRCurrency(product?.sellingPrice) }</p>
                                 <p className='text-slate-500 line-through'>{ displayINRCurrency(product?.price)  }</p>
                             </div>
-                            <button className='text-sm bg-gray-600 hover:bg-gray-700 text-white px-3 py-0.5 rounded-full' onClick={(e)=>handleAddToCart(e,product?._id)}>Add to Cart</button>
+                            <button className='text-sm bg-gray-600 hover:bg-gray-700 text-white px-3 m-2 py-0.5 rounded-full' onClick={(e)=>handleAddToCart(e,product?._id)}>Add to Cart</button>
                         </div>
                     </Link>
                 )
